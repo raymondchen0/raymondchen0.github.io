75d8c54c0e0ef4365e838df493a3b972bbe401d8
raymondchen0 <43011283+raymondchen0@users.noreply.github.com>
2020-09-12 14:00:37 -0400

    Add files via upload

---
+++ Allenspond/main.js
@@ -0,0 +1,28 @@
+
+
+
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-200px";
+  }
+  prevScrollpos = currentScrollPos;
+}
+
+
+
+
+function openNav() {
+  document.getElementById("mySidenav").style.width = "100%";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
+
+
+
+// slider
